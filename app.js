b67abf868e276824d70e5b4fb234bf805f5645aa
Felipeopazoc <felipeopazo2409@gmail.com>
2022-11-09 16:14:25 -0300

    añadiendo webhooks

--- app.js
+++ app.js
@@ -17,3 +17,24 @@ app.get("/",(req,res)=>{
 app.post("/webhook",(req,res)=>{
     console.log(req.body);
 })
+app.get("/webhook",(req,res)=>{
+    const verify_token = "felipeopazo2409";
+
+    res.json(req.body);
+    const mode = req.body["hub.mode"];
+    const token = req.body["hub.verify_token"];
+    const challenge = req.body["hub.challenge"];
+   
+    // Check if a token and mode were sent
+    if (mode && token) {
+        // Check the mode and token sent are correct
+        if (mode === "subscribe" && token === verify_token) {
+        // Respond with 200 OK and challenge token from the request
+        console.log("WEBHOOK_VERIFIED",challenge);
+        //res.status(200).send(challenge);
+        } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+            console.log("WEBHOOK no verificado");
+        }
+    }
+})
