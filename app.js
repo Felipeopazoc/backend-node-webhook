767749077d6be9770ba38e3ef38983df32635540
Felipeopazoc <felipeopazo2409@gmail.com>
2022-11-09 17:33:57 -0300

    webhook-verify

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/webhook",(req,res)=>{
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
     // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === config.verifyToken) {
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
