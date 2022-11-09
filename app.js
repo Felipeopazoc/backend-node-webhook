5a5451a97e57804211d809fb268e3e511913a72c
Felipeopazoc <felipeopazo2409@gmail.com>
2022-11-09 15:58:18 -0300

    version mejorada

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.listen(3000,()=>{
     console.log("El servidor esta activo");
 });
 
+app.get("/",(req,res)=>{
+    res.json("Hola");
+});
 
 app.post("/webhook",(req,res)=>{
     console.log(req.body);
