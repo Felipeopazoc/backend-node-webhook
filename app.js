fcc75986a9c86b534833d0ebc85bc7c2b684d21f
Felipeopazoc <felipeopazo2409@gmail.com>
2022-11-09 16:09:59 -0300

    servidor modificado

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
 const express = require("express");
 
+const port = process.env.PORT || 3000;
+
 const app = express()
-app.use(express.json())
-app.listen(3000,()=>{
+app.use(express.json());
+
+app.listen(port,()=>{
     console.log("El servidor esta activo");
 });
 
